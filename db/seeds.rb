4af290630455713654eee25342a75e524e5a8497
Ally Conway <avconway@hotmail.co.uk>
2018-12-08 16:36:08 +0000

    Creates SqlRunner class and CRUD methods for member class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,36 @@
 require_relative('../models/booking.rb')
 require_relative('../models/gymclass.rb')
 require_relative('../models/member.rb')
+require_relative('../models/workout.rb')
 require('pry')
 
+Member.delete_all()
+
 member1 = Member.new({
-  'member_name' => 'Joe Bloggs',
-  'email' => 'jb@mail.com',
-  'member_since' => '01-03-2015',
-  'membership_type' => 'standard'
+  'member_name' => 'Sylvester Stallone',
+  'email' => 'sly@mail.com',
+  'premium_membership' => 'true'
+})
+member1.save()
+
+member2 = Member.new({
+  'member_name' => 'Dolph Lundgren',
+  'email' => 'DL@mail.com',
+  'premium_membership' => 'false'
+})
+member2.save()
+
+workout1 = Workout.new({
+  'workout_name' => 'Boxfit',
+  'workout_type' => 'cardio'
 })
 
 gymclass1 = GymClass.new({
   'workout_id' =>  '4',
-  'date' => '10-Dec-2018',
-  'time' => '19.00',
+  'start_date' => '10-Dec-2018',
+  'start_time' => '19.00',
   'capacity' => '5',
-  'peak' => 'peak'
+  'peak' => 'false'
 })
 
 booking1 = Booking.new({
--- models/member.rb
+++ models/member.rb
@@ -12,7 +12,61 @@ class Member
     @premium_membership = options['premium_membership']
   end
 
+  # CREATE
+  def save()
+    sql = "INSERT INTO members (member_name, email, premium_membership)
+    VALUES ($1, $2, $3) returning id"
+    values = [@member_name, @email, @premium_membership]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i()
+  end
 
+  # READ all
+  def self.all()
+    sql = "SELECT * FROM members"
+    member_data = SqlRunner.run(sql)
+    return Member.map_items(member_data)
+  end
+
+  #READ by id
+  def self.find(id)
+    sql = "SELECT * FROM members WHERE id = $1"
+    values = [id]
+    member_data = SqlRunner.run(sql, values)
+    return Member.map_item(member_data)
+  end
+
+  #UPDATE
+  def update()
+    sql = "UPDATE members SET (member_name, email, premium_membership)
+    = ($1, $2, $3) WHERE id = $4"
+    values = [@member_name, @email, @premium_membership, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  #DELETE all
+  def self.delete_all()
+    sql = "DELETE FROM members"
+    SqlRunner.run(sql)
+  end
+
+  #DELETE by id
+  def delete()
+    sql = "DELETE FROM members WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  #Helper methods for mapping
+  def self.map_items(member_data)
+    result = member_data.map {|member| Member.new(member)}
+    return result
+  end
+
+  def self.map_item(member_data)
+    result = Member.map_items(member_data)
+    return result.first
+  end
 
 
 
