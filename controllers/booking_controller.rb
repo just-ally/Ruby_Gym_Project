65ea4381e3912c040041ef9d507f12658afa9916
Ally Conway <avconway@hotmail.co.uk>
2018-12-09 13:21:59 +0000

    Adds views for members pages

--- controllers/booking_controller.rb
+++ controllers/booking_controller.rb
@@ -4,6 +4,7 @@ require_relative('../models/booking.rb')
 also_reload('../models/*')
 
 get '/bookings' do
+  @bookings = Booking.all()
   erb(:"bookings/index")
 end
 
--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -8,6 +8,11 @@ get '/members' do
   erb(:"members/index")
 end
 
+get '/members/:id' do
+  @member = Member.find(params['id'].to_i)
+  erb(:"members/show")
+end
+
 get '/members/new' do
   erb(:"members/new")
 end
