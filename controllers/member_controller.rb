873116229f39b2b5e54b94853e1b0bd73de18638
Ally Conway <avconway@hotmail.co.uk>
2018-12-09 16:09:30 +0000

    Adds current bookings table to member page

--- controllers/member_controller.rb
+++ controllers/member_controller.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/member.rb')
+require_relative('../models/workout.rb')
 also_reload('../models/*')
 
 get '/members' do
@@ -10,6 +11,8 @@ end
 
 get '/members/:id' do
   @member = Member.find(params['id'].to_i)
+  @workouts = @member.workouts()
+  @gymclasses = @member.gymclasses()
   erb(:"members/show")
 end
 
